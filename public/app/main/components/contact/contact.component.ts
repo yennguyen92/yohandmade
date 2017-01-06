7e14b3f041d045e77a7466d3007e3a053b109825
Hiep Cao <hcao7@csc.com>
2017-01-06 13:19:28 +0700

    clean

--- public/app/main/components/contact/contact.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, AfterContentInit } from '@angular/core';
-
-@Component({
-    templateUrl: 'app/main/components/contact/contact.component.html',
-    styleUrls: ['app/main/components/contact/contact.component.css']
-})
-export class ContactComponent{
-
-}
